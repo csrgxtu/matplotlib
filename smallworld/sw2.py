9d37f5f0adba6e273ddbecfd7225e244b4a66286
archer <1246506786@qq.com>
2016-03-07 23:14:37 +0800

    fuck fuck

--- smallworld/sw2.py
+++ smallworld/sw2.py
@@ -34,10 +34,14 @@ list_y2_axis = list_y2_axis[1:]
 plt.xlim(-1.5,1.5)
 plt.ylim(-1.5,1.5)
 
-G = nx.Graph()
+# G = nx.Graph()
+G=nx.grid_graph(dim=[6,6])
+
 nodes = range(1, 19)
 G.add_nodes_from(nodes)
 pos = nx.spring_layout(G)
+# pos=nx.spring_layout(G,iterations=100)
+
 for i in range(1, 19):
     pos[i] = array([list_x2_axis[i - 1], list_y2_axis[i - 1]])
 
@@ -60,9 +64,29 @@ edges.append((16, 4))
 edges.append((18, 7))
 edges.append((1, 8))
 
-nx.draw_networkx_nodes(G, pos, nodelist=nodes, node_color='k', node_size=50, alpha=1)
-nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
-nx.draw_networkx_edges(G, pos, edgelist=edges, width=1.0, alpha=1)
+plt.subplot(221)
+# plt.subplot(111)
+
+plt.axis('off')
+npos = {}
+for key, val in pos.items():
+    # print type(key)
+    if type(key) == int:
+        # print key, val
+        npos[key] = array(val)
+nx.draw_networkx_nodes(G, npos, nodelist=nodes, node_color='k', node_size=50, alpha=1)
+# nx.draw_networkx_edges(G, npos, width=1.0, alpha=0.5)
+nx.draw_networkx_edges(G, npos, edgelist=edges, width=1.0, alpha=1)
+
+plt.subplot(222)
+plt.axis('off')
+nx.draw_networkx_nodes(G, npos, nodelist=nodes, node_color='k', node_size=50, alpha=1)
+nx.draw_networkx_edges(G, npos, edgelist=edges, width=1.0, alpha=1)
+
+plt.subplot(223)
+plt.axis('off')
+nx.draw_networkx_nodes(G, npos, nodelist=nodes, node_color='k', node_size=50, alpha=1)
+nx.draw_networkx_edges(G, npos, edgelist=edges, width=1.0, alpha=1)
 
 # show the plot
 plt.show()
