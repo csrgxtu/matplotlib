1596ac09f504f8dd61ce1310a8d92620cc9d603c
Archer Reilly <1246506786@qq.com>
2016-03-15 08:42:44 +0800

    temp commit

--- coordinates/coordinates.py
+++ coordinates/coordinates.py
@@ -12,12 +12,16 @@ import matplotlib.pyplot as plt
 def func(x):
   return (0.167 * 0.167 - 0.167 * 0.167 * x) / (2 * x * x)
 
+def funca(gamma, x):
+  return (gamma * gamma - gamma * gamma * x) / (2 * x * x)
+
 def run():
   X = [x/10.0 for x in range(1, 11, 1)]
   Y = []
 
   for x in X:
-    Y.append(func(x))
+    # Y.append(func(x))
+    Y.append(funca(0.015625, x))
 
   # print X
   # print Y
